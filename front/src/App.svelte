970d469451c73463c8044891b2fd33f7e260adc9
crosstyan <crosstyan@outlook.com>
2024-03-19 02:07:27 +0800

    comments

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -48,6 +48,8 @@
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
     // might be set a fixed frame rate or leave it empty to calculate it dynamically
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/captureStream
+    // https://developer.chrome.com/blog/capture-stream
     const stream = canvas.captureStream();
     videoElem.srcObject = stream;
     conn.onmessage = (e) => {
@@ -56,20 +58,28 @@
         const dim_info = unmarshal_image_dim_info(
           data.slice(0, image_dim_info_size_needed),
         );
-        canvas.width = dim_info.width;
-        canvas.height = dim_info.height;
+        if (
+          canvas.width !== dim_info.width ||
+          canvas.height !== dim_info.height
+        ) {
+          canvas.width = dim_info.width;
+          canvas.height = dim_info.height;
+        }
         video_dim = dim_info;
         const img_data = new Uint8ClampedArray(
           data.slice(image_dim_info_size_needed),
         );
+        // https://developer.mozilla.org/en-US/docs/Web/API/ImageData
         const img = ctx?.createImageData(dim_info.width, dim_info.height);
         if (img) {
           for (let i = 0, j = 0; i < img_data.length; i += 3, j += 4) {
             img.data[j] = img_data[i];
             img.data[j + 1] = img_data[i + 1];
             img.data[j + 2] = img_data[i + 2];
+            // image data is always 4 bytes per pixel (the last byte is alpha channel, set to 255 for opaque image)
             img.data[j + 3] = 255;
           }
+          // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/putImageData
           ctx?.putImageData(img, 0, 0);
         }
       }
