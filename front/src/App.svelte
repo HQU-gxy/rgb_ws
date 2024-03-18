975e61ea29c9caa070bb79c2d1f1284aff52bd1c
crosstyan <crosstyan@outlook.com>
2024-03-18 18:41:47 +0800

    front end and python memory leak

---
+++ front/src/App.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  // https://docs.opencv.org/4.x/d3/d63/classcv_1_1Mat.html
+  enum BitDepth {
+    UINT8 = 0,
+    INT8 = 1,
+    UINT16 = 2,
+    INT16 = 3,
+    INT32 = 4,
+    FLOAT32 = 5,
+    FLOAT64 = 6,
+    FLOAT16 = 7,
+  }
+
+  interface ImageDimensions {
+    width: number;
+    height: number;
+    stride: number;
+    channels: number;
+    bit_depth: BitDepth;
+  }
+
+  let video_dim: ImageDimensions | undefined = undefined;
+
+  const unmarshal_image_dim_info = (data: ArrayBufferLike): ImageDimensions => {
+    const view = new DataView(data);
+    const width = view.getUint16(0, false);
+    const height = view.getUint16(2, false);
+    const stride = view.getUint16(4, false);
+    const channels = view.getUint8(6);
+    const bit_depth = view.getUint8(7);
+    return { width, height, stride, channels, bit_depth };
+  };
+
+  const image_dim_info_size_needed = 8;
+
+  const ws_addr = "ws://192.168.2.228:8000";
+
+  let videoElem: HTMLVideoElement;
+
+  onMount(() => {
+    const conn = new WebSocket(ws_addr);
+    conn.binaryType = "arraybuffer";
+    conn.onopen = () => {
+      console.info("WebSocket connection established");
+    };
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    // might be set a fixed frame rate or leave it empty to calculate it dynamically
+    const stream = canvas.captureStream();
+    videoElem.srcObject = stream;
+    conn.onmessage = (e) => {
+      const data = e.data;
+      if (data instanceof ArrayBuffer) {
+        const dim_info = unmarshal_image_dim_info(
+          data.slice(0, image_dim_info_size_needed),
+        );
+        canvas.width = dim_info.width;
+        canvas.height = dim_info.height;
+        video_dim = dim_info;
+        const img_data = new Uint8ClampedArray(
+          data.slice(image_dim_info_size_needed),
+        );
+        const img = ctx?.createImageData(dim_info.width, dim_info.height);
+        if (img) {
+          for (let i = 0, j = 0; i < img_data.length; i += 3, j += 4) {
+            img.data[j] = img_data[i];
+            img.data[j + 1] = img_data[i + 1];
+            img.data[j + 2] = img_data[i + 2];
+            img.data[j + 3] = 255;
+          }
+          ctx?.putImageData(img, 0, 0);
+        }
+      }
+    };
+    return () => {
+      conn.close();
+    };
+  });
+</script>
+
+<main>
+  <div class="bg-lime-400">test</div>
+  {#if video_dim}
+    <p>
+      {video_dim.width}x{video_dim.height}
+    </p>
+  {/if}
+  <video
+    id="the-video"
+    autoplay
+    controls
+    bind:this={videoElem}
+    width={video_dim ? video_dim.width : 0}
+    height={video_dim ? video_dim.height : 0}
+  ></video>
+</main>
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.websockets import WebSocket
 from starlette.routing import Route, WebSocketRoute
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 from starlette.concurrency import run_until_first_complete, run_in_threadpool
 from broadcaster import Broadcast
 import orjson as json
@@ -14,7 +18,6 @@ import anyio
 from anyio import create_memory_object_stream
 from anyio.streams.memory import MemoryObjectReceiveStream
 from pydantic import BaseModel
-import starlette
 import uvicorn
 import logging
 import contextlib
@@ -22,6 +25,9 @@ import click
 import struct
 from typing import Optional, TypeVar, Generic, cast, Any
 from enum import Enum, auto
+from time import time
+
+send_stream, receive_stream = create_memory_object_stream(0, bytes)
 
 
 class BitDepth(Enum):
@@ -53,7 +59,7 @@ class ImageDimension(BaseModel):
     width: int
     height: int
     stride: int
-    channel: int
+    channels: int
     bit_depth: BitDepth
 
     @staticmethod
@@ -62,7 +68,7 @@ class ImageDimension(BaseModel):
 
     def marshal(self) -> bytes:
         return struct.pack(self.struct_format(), self.width, self.height,
-                           self.stride, self.channel, self.bit_depth.value)
+                           self.stride, self.channels, self.bit_depth.value)
 
     @staticmethod
     def bytes_size() -> int:
@@ -78,7 +84,7 @@ class ImageDimension(BaseModel):
         return ImageDimension(width=width,
                               height=height,
                               stride=stride,
-                              channel=channel,
+                              channels=channel,
                               bit_depth=BitDepth(bit_depth))
 
     @staticmethod
@@ -87,11 +93,11 @@ class ImageDimension(BaseModel):
         return ImageDimension(width=mat.shape[1],
                               height=mat.shape[0],
                               stride=mat.strides[0],
-                              channel=mat.shape[2],
+                              channels=mat.shape[2],
                               bit_depth=bit_depth)
 
 
-SHOW_WINDOW = False
+# can't use cv.imshow in another thread
 CHANNEL_NAME = "video"
 T = TypeVar("T")
 
@@ -123,12 +129,15 @@ async def ws_sender(websocket: WebSocket):
 
 # https://anyio.readthedocs.io/en/stable/streams.html
 async def ws_handler(ws: WebSocket):
-    await ws.accept()
-    await run_until_first_complete((ws_receiver, {
-        "websocket": ws
-    }), (ws_sender, {
-        "websocket": ws
-    }))
+    try:
+        await ws.accept()
+        await run_until_first_complete((ws_receiver, {
+            "websocket": ws
+        }), (ws_sender, {
+            "websocket": ws
+        }))
+    except Exception as e:
+        logger.error(e)
 
 
 @contextlib.asynccontextmanager
@@ -146,15 +155,58 @@ async def lifespan(_app: Starlette):
     logger.info("lifespan end")
 
 
-def run_video_cap():
+EXPECTED_WIDTH = 640
+EXPECTED_HEIGHT = 480
+
+
+class SimpleMovingAverage:
+    _sum: float = 0.0
+    _count: int = 0
+    _max_count: int = 10
+    _value: Optional[float] = None
+
+    def __init__(self, max_count: int = 10):
+        self._max_count = max_count
+        self._sum = 0.0
+        self._count = 0
+        self._value = None
+
+    @property
+    def value(self) -> Optional[float]:
+        return self._value
+
+    def next(self, val: float) -> float:
+        if self._count < self._max_count:
+            self._sum += val
+            self._count += 1
+            self._value = self._sum / self._count
+        else:
+            self._sum -= self._sum / self._max_count
+            self._sum += val
+            self._value = self._sum / self._max_count
+        return self._value
+
+    def reset(self):
+        self._sum = 0.0
+        self._count = 0
+        self._value = None
+
+
+async def run_video_cap():
     # use GStreamer to get a video stream from the test video
     # https://github.com/opencv/opencv/blob/625eebad54a34a7bdad6812f3e9ec050a1b3adc5/modules/videoio/src/cap_gstreamer.cpp#L1342-L1344
     # https://stackoverflow.com/questions/51213730/how-to-get-gstreamer-live-stream-using-opencv-and-python
     from loguru import logger
     # make sure GStreamer is Yes in Video I/O
     logger.info(cv2.getBuildInformation())
-    pipeline = "videotestsrc is-live=true ! timeoverlay ! videoconvert ! appsink name=opencvsink"
-    cap = cv.VideoCapture(pipeline, cv.CAP_GSTREAMER)
+    # pipeline = "videotestsrc is-live=true ! timeoverlay ! videoconvert ! appsink name=opencvsink"
+    # cap = cv.VideoCapture(pipeline, cv.CAP_GSTREAMER)
+    cap = cv.VideoCapture(1, cv.CAP_AVFOUNDATION)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, EXPECTED_WIDTH)
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, EXPECTED_HEIGHT)
+    cap.set(cv.CAP_PROP_FPS, 30)
+    process_time_sma = SimpleMovingAverage(30)
+    frame_count = 0
     if not cap.isOpened():
         logger.error("capture is not opened. Exiting ...")
         return
@@ -165,18 +217,22 @@ def run_video_cap():
                 logger.error("Can't receive frame (stream end?). Exiting ...")
                 break
             # cv.imshow("frame", frame)
+            start = time()
+            if frame.shape[1] > EXPECTED_WIDTH or frame.shape[
+                    0] > EXPECTED_HEIGHT:
+                frame = cv.resize(frame, (EXPECTED_WIDTH, EXPECTED_HEIGHT))
             rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
             global broadcast
-            broadcast = cast(Broadcast, broadcast)
-            # TODO: a struct/header for stride, width, height, frame info, etc.
-            info = ImageDimension.from_cv_mat(rgb)
-            anyio.run(broadcast.publish, CHANNEL_NAME, info.marshal() + rgb.tobytes())
-            if SHOW_WINDOW:
-                cv.namedWindow("frame", cv.WINDOW_NORMAL)
-                cv.imshow("frame", frame)
-                k = cv.waitKey(1)
-                if k == ord('q'):
-                    break
+            if broadcast is not None:
+                info = ImageDimension.from_cv_mat(rgb)
+                await broadcast.publish(CHANNEL_NAME,
+                                        info.marshal() + rgb.tobytes())
+
+                diff = time() - start
+                process_time_sma.next(diff)
+            frame_count += 1
+            if frame_count % 30 == 0:
+                logger.info(f"process time: {process_time_sma.value}")
 
     except KeyboardInterrupt:
         cap.release()
@@ -184,24 +240,34 @@ def run_video_cap():
     except Exception as e:
         cap.release()
         cv.destroyAllWindows()
-        print(e)
+        logger.error(e)
 
 
 @click.command()
 @click.option("--port", default=8000, help="Port number")
 @click.option("--host", default="0.0.0.0", help="Host")
 def main(port: int, host: str):
-    video_thread = Thread(target=run_video_cap)
-    app = Starlette(
-        debug=True,
-        routes=[
-            Route('/', lambda _request: JSONResponse({'test': 'test'})),
-            # I'm not sure what the name is used for the websocket endpoint
-            WebSocketRoute('/', ws_handler, name='ws'),
-        ],
-        lifespan=lifespan)  # type: ignore
+    video_thread = Thread(target=lambda: anyio.run(run_video_cap))
+    middleware = [
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        ),
+    ]
+    app = Starlette(debug=True,
+                    routes=[
+                        Route('/',
+                              lambda _request: JSONResponse({'test': 'test'})),
+                        WebSocketRoute('/', ws_handler, name='ws'),
+                    ],
+                    middleware=middleware,
+                    lifespan=lifespan)  # type: ignore
     video_thread.start()
     uvicorn.run(app, host=host, port=port)
+    video_thread.join()
 
 
 if __name__ == "__main__":
